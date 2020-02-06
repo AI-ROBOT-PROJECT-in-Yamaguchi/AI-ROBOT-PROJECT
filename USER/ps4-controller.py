3bee1e5659b6fdc54d9f3e4dcfd57a9cd4ec2213
jnmk929 <amour1125.j.0609@gmail.com>
2020-02-06 15:53:59 +0900

    add input data assign comment

--- USER/ps4-controller.py
+++ USER/ps4-controller.py
@@ -1,3 +1,32 @@
+#input id 
+#
+#button_data   (push = true release = false)
+#   0 : X   
+#   1 : O 
+#   2 : triangle  
+#   3 : square  
+#   4 : L1 
+#   5 : R1 
+#   6 : L2 
+#   7 : R2 
+#   8 : SHARE 
+#   9 : OPTIONS
+#   10: PS 
+#   11: push left joystick
+#   12: push right joystick
+#
+#axis_data
+#   0 : left joystick x     (left = -1 < x < 1 = right)
+#   1 : left joystick y     (up = -1 < y < 1 = down)
+#   2 : L2                  (push = 1.0 <   < -1.0 = release)
+#   3 : right joystick x    (left = -1 < x < 1 = right)
+#   4 : right joystick y    (up = -1 < y < 1 = down)
+#   5 : R2                  (push = 1.0 <   < -1.0 = release)
+#
+#hat_data
+#   0 : 0 : x (left = -1.0,right = 1.0)
+#       1 : y (up = 1.0, down = -1.0)
+
 import os
 import pygame
 import math
@@ -44,27 +73,28 @@ class PS4Controller(object):
                 #print(self.button_data)
                 #print(self.axis_data)
                 #print(self.hat_data)
-            if len(self.axis_data) == 4:
+
+            if len(self.axis_data) >= 4:
                 #left joystick
-                if self.distance(0,1) > 0.95 and self.distance(0,1) < 1.05:
+                if self.distance(0,1) > 0.75 and self.distance(0,1) < 1.25:
                     left_angle = self.left_controller_angle()
                 else :
                     left_angle = -1
 
                 #right joystick 
-                if self.distance(3,4) > 0.95 and self.distance(3,4) < 1.05:
+                if self.distance(3,4) > 0.75 and self.distance(3,4) < 1.25:
                     right_angle = self.right_controller_angle()
                 else:
                     right_angle = -1
                 print("left:",left_angle,"right:",right_angle)
-
+            
     def left_controller_angle(self):
         x_deg = math.degrees(math.acos(self.axis_data[0]))
         y_deg = math.degrees(math.asin(self.axis_data[1]))
         if y_deg < 0:
             return x_deg
         else:
-            return 360 - x_deg
+            return 360.0 - x_deg
 
     def right_controller_angle(self):
         x_deg = math.degrees(math.acos(self.axis_data[3]))
@@ -72,7 +102,7 @@ class PS4Controller(object):
         if y_deg < 0:
             return x_deg
         else:
-            return 360 - x_deg
+            return 360.0 - x_deg
         
     
     def distance(self,x,y):
