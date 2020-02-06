e467f2cda91d5248fa55d325d70acac9674197ba
jnmk929 <amour1125.j.0609@gmail.com>
2020-02-06 19:02:21 +0900

    add rotate command

--- USER/ps4-controller.py
+++ USER/ps4-controller.py
@@ -24,18 +24,22 @@
 #   5 : R2                  (push = 1.0 <   < -1.0 = release)
 #
 #hat_data
-#   0 : 0 : x (left = -1.0,right = 1.0)
+#   0 : 0 : x (left = -1.0, right = 1.0)
 #       1 : y (up = 1.0, down = -1.0)
 
 import os
 import pygame
 import math
+import RPi.GPIO as GPIO
+import numpy as np
 
 class PS4Controller(object):
     controller = None
     axis_data = None
     button_data = None
     hat_data = None
+    right_angle = -1.0
+    left_angle = -1.0
 
     def __init__(self):
         pygame.init()
@@ -44,8 +48,6 @@ class PS4Controller(object):
         self.controller.init()
 
     def listen(self):
-        right_angle = 0.0
-        left_angle = 0.0
         if not self.axis_data:
             self.axis_data = {}
 
@@ -77,16 +79,21 @@ class PS4Controller(object):
             if len(self.axis_data) >= 4:
                 #left joystick
                 if self.distance(0,1) > 0.75 and self.distance(0,1) < 1.25:
-                    left_angle = self.left_controller_angle()
+                    self.left_angle = self.left_controller_angle()
                 else :
-                    left_angle = -1
+                    self.left_angle = -1.0
 
                 #right joystick 
                 if self.distance(3,4) > 0.75 and self.distance(3,4) < 1.25:
-                    right_angle = self.right_controller_angle()
+                    self.right_angle = self.right_controller_angle()
                 else:
-                    right_angle = -1
-                print("left:",left_angle,"right:",right_angle)
+                    self.right_angle = -1.0
+            #motor command
+            if not self.left_angle == -1.0:
+                self.send_leftjoy_command(0,1,2,3)
+
+            if not self.right_angle == -1.0:
+                self.send_rightjoy_command(0,1,2,3)
             
     def left_controller_angle(self):
         x_deg = math.degrees(math.acos(self.axis_data[0]))
@@ -103,12 +110,52 @@ class PS4Controller(object):
             return x_deg
         else:
             return 360.0 - x_deg
-        
-    
+         
     def distance(self,x,y):
         return math.sqrt(pow(0-self.axis_data[x],2)+pow(0-self.axis_data[y],2))
+        
+    #rotate
+    def send_leftjoy_command(self,m1,m2,m3,m4):
+        #right side
+        rad = math.radians(self.left_angle)
+        if math.cos(rad) >= math.sqrt(3)/2:
+            rate = 7.1*math.cos(rad)-6.1  # 0 ~ 1 speed rate near 0
+            speed = rate * 255
+            print("right_rotate:",speed)
+            #GPIO.output(m1, speed)
+            #GPIO.output(-m2, speed)
+            #GPIO.output(m3, speed)
+            #GPIO.output(-m4, speed)
+        #left side
+        elif math.cos(rad) <= -math.sqrt(3)/2:
+            rate = 7.1*-math.cos(rad)-6.1  # 0 ~ 1 speed rate near 180
+            speed = rate * 255
+            print("left_rotate:",speed)
+            #GPIO.output(-m1, speed)
+            #GPIO.output(m2, speed)
+            #GPIO.output(-m3, speed)
+            #GPIO.output(m4, speed)
+    #move
+    """
+    def send_rightjoy_command(self,m1,m2,m3,m4):
+        rad = math.radians(self.right_angle)
 
-
+        inv_b = np.array([[-math.cos(rad),-math.sin(rad)],
+                        [ math.cos(rad), math.sin(rad)],
+                        [ math.sin(rad),-math.cos(rad)],
+                        [-math.sin(rad), math.cos(rad)]])
+        v = np.array([math.cos(rad)*255,math.sin(rad)*255])
+        
+        speed = np.dot(inv_b,v)
+        print("m1",-speed[0])
+        print("m2",speed[1])
+        print("m3",speed[2])
+        print("m4",-speed[3])
+        #GPIO.output(m1, speed[0])
+        #GPIO.output(m2, -speed[1]) 
+        #GPIO.output(m3, -speed[2])
+        #GPIO.output(m4, speed[3])
+    """
 if __name__ == "__main__":
     ps4 = PS4Controller()
     ps4.listen()
