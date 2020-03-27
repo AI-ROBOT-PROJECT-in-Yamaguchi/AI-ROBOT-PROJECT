bac5b0478e4ed2cd6c318dc26eee9289b6172194
Taiki Tomita <g.tomita.t@gmail.com>
2020-03-27 15:42:28 +0900

    ghplay.py 関数として呼び出せるようにした。

--- common/smartspeaker/ghplay.py
+++ common/smartspeaker/ghplay.py
@@ -2,9 +2,11 @@ import pychromecast
 import sys
 from ghconfig import *
 
-mediaUrl = sys.argv[1]
+def play(mediaUrl):
+    googleHome = pychromecast.Chromecast(googleHomeAddress)
+    googleHome.wait()
+    googleHome.media_controller.play_media(mediaUrl+"?text="+text+"&lang="+lang,mediaType)
+    googleHome.media_controller.block_until_active()
 
-googleHome = pychromecast.Chromecast(googleHomeAddress)
-googleHome.wait()
-googleHome.media_controller.play_media(mediaUrl+"?text="+text+"&lang="+lang,mediaType)
-googleHome.media_controller.block_until_active()
+if __name__=="__main__":
+    play(sys.argv[1])
