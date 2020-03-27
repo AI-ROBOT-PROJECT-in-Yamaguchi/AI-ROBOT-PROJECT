af22818d469c26ca86bccdda894b3d1fe6a7e1e8
Taiki Tomita <g.tomita.t@gmail.com>
2020-03-27 15:42:44 +0900

    ghtalk.py 実装

--- common/smartspeaker/ghconfig.py
+++ common/smartspeaker/ghconfig.py
@@ -1,2 +1,3 @@
+mediaUrl = "http://example.com/output.mp3&text=%s&lang=ja"
 mediaType = "audio/mp3"
 googleHomeAddress="192.168.43.167"
---
+++ common/smartspeaker/ghtalk.py
@@ -0,0 +1,5 @@
+import sys
+import ghplay
+from ghconfig import *
+
+ghplay.play(mediaUrl%(sys.argv[1]))
